0a9c1e7f30a0c4c9deb2391efa9838f5d6d81c1a
Aviv Haruzi <avivharuzi@gmail.com>
2020-02-22 12:10:29 +0200

    Add material module

---
+++ client/src/app/material/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+  ]
+})
+export class MaterialModule { }
